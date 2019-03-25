3f6a6eb60f31261950b5f500e174139a28c49388
Michael B <mboeynaems@outlook.com>
2019-03-25 08:07:08 +0100

    created more users

--- Identityserver/src/Configuration/Users.cs
+++ Identityserver/src/Configuration/Users.cs
@@ -41,6 +41,162 @@ namespace IdentityServer.Configuration
                         new Claim("location", "somewhere"),
                     }
                  },
+                 new TestUser{SubjectId = "50001", Username = "student1", Password = "Student",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Student 1"),
+                        new Claim(JwtClaimTypes.GivenName, "Student"),
+                        new Claim(JwtClaimTypes.FamilyName, "1"),
+                        new Claim(JwtClaimTypes.Email, "student1@example.org"),
+                        new Claim(JwtClaimTypes.Role, "Reviewer"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.example.org"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 2450, 'country': 'Belgium' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                 new TestUser{SubjectId = "50002", Username = "student2", Password = "Student",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Student 2"),
+                        new Claim(JwtClaimTypes.GivenName, "Student"),
+                        new Claim(JwtClaimTypes.FamilyName, ""),
+                        new Claim(JwtClaimTypes.Email, "student2@example.org"),
+                        new Claim(JwtClaimTypes.Role, "Reviewer"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.example.org"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 2450, 'country': 'Belgium' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                 new TestUser{SubjectId = "50003", Username = "student3", Password = "Student",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Student 3"),
+                        new Claim(JwtClaimTypes.GivenName, "Student"),
+                        new Claim(JwtClaimTypes.FamilyName, "3"),
+                        new Claim(JwtClaimTypes.Email, "student3@example.org"),
+                        new Claim(JwtClaimTypes.Role, "Reviewer"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.example.org"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 2450, 'country': 'Belgium' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                 new TestUser{SubjectId = "50004", Username = "student4", Password = "Student",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Student 4"),
+                        new Claim(JwtClaimTypes.GivenName, "Student"),
+                        new Claim(JwtClaimTypes.FamilyName, "4"),
+                        new Claim(JwtClaimTypes.Email, "student4@example.org"),
+                        new Claim(JwtClaimTypes.Role, "Reviewer"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.example.org"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 2450, 'country': 'Belgium' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                 new TestUser{SubjectId = "50005", Username = "student5", Password = "Student",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Student 5"),
+                        new Claim(JwtClaimTypes.GivenName, "Student"),
+                        new Claim(JwtClaimTypes.FamilyName, "5"),
+                        new Claim(JwtClaimTypes.Email, "student5@example.org"),
+                        new Claim(JwtClaimTypes.Role, "Reviewer"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.example.org"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 2450, 'country': 'Belgium' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                 new TestUser{SubjectId = "50006", Username = "student6", Password = "Student",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Student 6"),
+                        new Claim(JwtClaimTypes.GivenName, "Student"),
+                        new Claim(JwtClaimTypes.FamilyName, "6"),
+                        new Claim(JwtClaimTypes.Email, "student6@example.org"),
+                        new Claim(JwtClaimTypes.Role, "Reviewer"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.example.org"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 2450, 'country': 'Belgium' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                 new TestUser{SubjectId = "50007", Username = "student7", Password = "Student",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Student 7"),
+                        new Claim(JwtClaimTypes.GivenName, "Student"),
+                        new Claim(JwtClaimTypes.FamilyName, "7"),
+                        new Claim(JwtClaimTypes.Email, "student7@example.org"),
+                        new Claim(JwtClaimTypes.Role, "Reviewer"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.example.org"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 2450, 'country': 'Belgium' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                 new TestUser{SubjectId = "50008", Username = "student8", Password = "Student",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Student 8"),
+                        new Claim(JwtClaimTypes.GivenName, "Student"),
+                        new Claim(JwtClaimTypes.FamilyName, "8"),
+                        new Claim(JwtClaimTypes.Email, "student8@example.org"),
+                        new Claim(JwtClaimTypes.Role, "Reviewer"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.example.org"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 2450, 'country': 'Belgium' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                 new TestUser{SubjectId = "50009", Username = "student9", Password = "Student",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Student 9"),
+                        new Claim(JwtClaimTypes.GivenName, "Student"),
+                        new Claim(JwtClaimTypes.FamilyName, "9"),
+                        new Claim(JwtClaimTypes.Email, "student10@example.org"),
+                        new Claim(JwtClaimTypes.Role, "Reviewer"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.example.org"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 2450, 'country': 'Belgium' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                 new TestUser{SubjectId = "500010", Username = "student10", Password = "Student",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Student 10"),
+                        new Claim(JwtClaimTypes.GivenName, "Student"),
+                        new Claim(JwtClaimTypes.FamilyName, "10"),
+                        new Claim(JwtClaimTypes.Email, "student10@example.org"),
+                        new Claim(JwtClaimTypes.Role, "Reviewer"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.example.org"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 2450, 'country': 'Belgium' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                 new TestUser{SubjectId = "500011", Username = "student11", Password = "Student",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Student 11"),
+                        new Claim(JwtClaimTypes.GivenName, "Student"),
+                        new Claim(JwtClaimTypes.FamilyName, "11"),
+                        new Claim(JwtClaimTypes.Email, "student11@example.org"),
+                        new Claim(JwtClaimTypes.Role, "Reviewer"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.example.org"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 2450, 'country': 'Belgium' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
+                 new TestUser{SubjectId = "500012", Username = "student12", Password = "Student",
+                    Claims =
+                    {
+                        new Claim(JwtClaimTypes.Name, "Student 12"),
+                        new Claim(JwtClaimTypes.GivenName, "Student"),
+                        new Claim(JwtClaimTypes.FamilyName, "12"),
+                        new Claim(JwtClaimTypes.Email, "student12@example.org"),
+                        new Claim(JwtClaimTypes.Role, "Reviewer"),
+                        new Claim(JwtClaimTypes.EmailVerified, "true", ClaimValueTypes.Boolean),
+                        new Claim(JwtClaimTypes.WebSite, "https://www.example.org"),
+                        new Claim(JwtClaimTypes.Address, @"{ 'street_address': 'One Hacker Way', 'locality': 'Heidelberg', 'postal_code': 2450, 'country': 'Belgium' }", IdentityServer4.IdentityServerConstants.ClaimValueTypes.Json)
+                    }
+                },
             };
         }
     }
